f525e18290b7f6052f81661c1502fe4997a93102
OwenZhu <398094982@qq.com>
2019-07-18 23:34:42 +1000

    init app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css'
+
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +24,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
+
+var myNewP = document.createElement("p")
+myNewP.innerHTML = "This is a paragraph."
 
 export default App;
